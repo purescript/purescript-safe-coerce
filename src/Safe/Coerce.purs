91699d055e29be750f028677f4bb1b9dafd67eda
Jordan Martinez <jordanalex.martinez@gmail.com>
2020-10-11 14:14:18 -0700

    Port unsafe-coerce docs to coerce

--- src/Safe/Coerce.purs
+++ src/Safe/Coerce.purs
@@ -6,5 +6,15 @@ module Safe.Coerce
 import Prim.Coerce (class Coercible)
 import Unsafe.Coerce (unsafeCoerce)
 
+-- | One application for this function is to avoid doing work that you know is a
+-- | no-op because of newtypes. For example, if you have an `Array (Conj a)` and you
+-- | want an `Array (Disj a)`, you could do `Data.Array.map (runConj >>> Disj)`, but
+-- | this performs an unnecessary traversal. `coerce` accomplishes the same
+-- | for free.
+-- |
+-- | ```purescript
+-- | mapConjToDisj :: forall a. Array (Conj a) -> Array (Disj a)
+-- | mapConjToDisj = coerce
+-- | ```
 coerce :: forall a b. Coercible a b => a -> b
 coerce = unsafeCoerce
